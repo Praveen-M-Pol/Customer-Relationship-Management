91ec8b18c29d4469a491271cdffc4a88846afa2a
Praveen Pol <praveenpol99@gmail.com>
2020-10-05 19:26:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-            <a class="navbar-brand text-danger font-weight-bold" href="#home">Customer Relationship System</a>
+            <a class="navbar-brand text-danger font-weight-bold" href="#home">Customer Relationship Management</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
